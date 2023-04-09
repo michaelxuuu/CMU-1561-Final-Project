fe3c0e897e0efc13c05ce12fd9e149d913c3320e
michaelxuuu <xyjmichael@gmail.com>
2023-04-09 04:30:43 -0400

    first thread

--- src/main.c
+++ src/main.c
@@ -1,5 +1,12 @@
 #include <stdio.h>
+#include "uthread.h"
+
+void *func(void *arg) {
+    printf("asda\n");
+    return 0;
+}
 
 int main() {
-
+    uthread_create(func, 0);
+    for(;;);
 }
--- src/uthread.c
+++ src/uthread.c
@@ -5,6 +5,8 @@
 #include <unistd.h>
 #include <stdlib.h>
 #include <pthread.h>
+#include <stdio.h>
+#include <errno.h>
 #include <stdatomic.h>
 #include "x86_64.h"
 
@@ -12,23 +14,42 @@
 #define UTHREAD_ALIGNEMNT 16
 #define UTHREAD_STACK_SIZE (sizeof(char) * 1024 * 1024 * 8)
 
+enum {
+    UTHREAD_STATE_RUNNING,
+    UTHREAD_STATE_SLEEPING,
+    UTHREAD_STATE_ZOMBIE,
+};
+
+// uthread context
 struct uthread {
     uint32_t id;
     uint32_t state;
-    uint32_t padding[2];
     ucontext_t uc;
+};
+
+// uthread queue node
+struct node {
+    struct node *prev;
+    struct node *next;
+    struct uthread uthd;
+};
+
+struct worker_id {
+    pthread_t id1;
+    uint32_t id2;
 };
 
 // worker data
 struct worker {
+    struct worker_id ids;
     pthread_mutex_t mu;
-    struct uthread *queue;
+    struct node *queue;
     uint32_t qsize;
     uint32_t cur_pthread_id;
 };
+
 // worker ids (thread local)
-static __thread pthread_t worker_id1;   // pthread id
-static __thread uint32_t worker_id2;         // integer id (assigned by the run-time)
+static __thread struct worker_id worker_id;
 
 // run-time data
 static struct {
@@ -36,18 +57,36 @@ static struct {
     struct worker *worker_pool;
     uint32_t pool_size;
     atomic_uint next_uthread_id;
-    uint32_t *ids;
+    atomic_uint next_worker_id;
 } runtime;
 
 // uthread schedualer (installed as the SIGALARM handler)
-static void* scheduler(uint32_t signo, siginfo_t *si, void *cpu_con) {
-    
+static void scheduler(int signum, siginfo_t *si, void *context) {
+    ucontext_t *uc = (ucontext_t *) context;
+    uc = (ucontext_t *) context;
+    uc = (ucontext_t *) context;
+    mcontext_t mc = uc->uc_mcontext;
+    int a = worker_id.id2;
+    if (runtime.worker_pool[0].cur_pthread_id == 0) {
+        char *s1 = (char *)&runtime.worker_pool[0].queue[0].uthd.uc;
+        char *s2 = (char *)runtime.worker_pool[0].queue[0].uthd.uc.uc_mcontext;
+        for (int i = 0; i < sizeof(ucontext_t); i++)
+            ((char *)uc)[i] = s1[i];
+        for (int i = 0; i < sizeof(_STRUCT_MCONTEXT64); i++)
+            ((char *)mc)[i] = s2[i];
+        
+    }
 }
 
 // initailze a worker (used on creation)
 static void* worker_init(void *id) {
-    worker_id1 = pthread_self();
-    worker_id2 = *(uint32_t *)id;
+    worker_id.id1 = pthread_self();
+    worker_id.id2 = *(uint32_t *)id;
+    struct sigaction sa;
+    sa.sa_sigaction = scheduler;
+    sigemptyset(&sa.sa_mask);
+    sa.sa_flags = SA_SIGINFO;
+    sigaction(SIGUSR1, &sa, NULL);
     // busy waiting
     for (;;);
 }
@@ -58,20 +97,21 @@ static void runtime_init() {
     runtime.next_uthread_id = 0;
     // start as many cores as workers
     runtime.pool_size = (uint32_t)sysconf(_SC_NPROCESSORS_ONLN);
-    runtime.ids = malloc(sizeof(uint32_t) * runtime.pool_size);
     runtime.worker_pool = malloc(sizeof(struct worker) * runtime.pool_size);
+    runtime.next_worker_id = 0;
 
     for (size_t i = 0; i < runtime.pool_size; i++) {
-        runtime.ids[i] = i;
+        runtime.worker_pool[i].ids.id2 = i;
         runtime.worker_pool[i].qsize = 0;
         runtime.worker_pool[i].queue = 0;
         pthread_mutex_init(&runtime.worker_pool[i].mu, 0);
-        pthread_create(NULL, NULL, worker_init, &runtime.ids[i]);
+        pthread_create(&runtime.worker_pool[i].ids.id1, NULL, worker_init, &runtime.worker_pool[i].ids.id2);
     }
 }
 
+// clean up the resource allcoated for the uthread
 static void cleanup() {
-
+    for (;;);
 }
 
 // allocate resources for a new thread and returns its id
@@ -80,13 +120,37 @@ uint32_t uthread_create(void *(*func)(void *), void *arg) {
     if (!runtime.is_active)
         runtime_init();
 
-    struct uthread *newthread = aligned_alloc(UTHREAD_ALIGNEMNT, sizeof(struct uthread));
+    struct node *newnode = malloc(sizeof(struct node));
+    newnode->uthd.uc.uc_mcontext = malloc(sizeof(_STRUCT_MCONTEXT64));
     
-    mcontext_t mc = newthread->uc.uc_mcontext;
-    uint64_t *stack = malloc(UTHREAD_STACK_SIZE);
-    stack[0] = (uint64_t)cleanup;
-    mc_set_rsp(mc, (uint64_t)(stack + 1));
+    mcontext_t mc = newnode->uthd.uc.uc_mcontext;
+    uint64_t *stack = (uint64_t *)((uint64_t)malloc(UTHREAD_STACK_SIZE) + UTHREAD_STACK_SIZE);
+    *(--stack) = (uint64_t)cleanup;
+    mc_set_rsp(mc, (uint64_t)(stack));
     mc_set_rdi(mc, (uint64_t)arg);
     mc_set_rip(mc, (uint64_t)func);
-    newthread->id = runtime.next_uthread_id++;
+    newnode->uthd.id = runtime.next_uthread_id++;
+    newnode->uthd.state = UTHREAD_STATE_SLEEPING;
+
+    // uint32_t worker_id = runtime.next_worker_id;
+    // struct worker *worker = &runtime.worker_pool[worker_id];
+    struct worker *worker = &runtime.worker_pool[0];
+
+    pthread_mutex_lock(&worker->mu);
+    if (worker->qsize++ == 0) {
+        worker->queue = newnode;
+        newnode->next = newnode;
+        newnode->prev = newnode;
+    } else {
+        newnode->next = worker->queue;
+        newnode->prev = worker->queue->prev;
+        worker->queue->prev->next = newnode;
+        worker->queue->prev = newnode;
+        worker->queue = newnode;
+    }
+    pthread_mutex_unlock(&worker->mu);
+
+    // test
+    asm("de:");
+    pthread_kill(runtime.worker_pool[0].ids.id1, SIGUSR1);
 }
